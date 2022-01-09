101b826c1c14bfdfb85bcfbc9378c6b51d59adda
Elayaraja <raja5ramani@gmail.com>
2022-01-09 17:14:17 +0530

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,19 @@ const submitBtn = document.querySelector(".form__btn");
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 });
+
+//Observer
+const projects = document.querySelectorAll(".project");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show-grid", entry.isIntersecting);
+    });
+  },
+  { threshold: 0.5 }
+);
+
+projects.forEach((project) => {
+  observer.observe(project);
+});
